15bc996341f50eb988051679d7977f73d02969f4
Turar Arykbaev <turar.arykbaev@gmail.com>
2020-04-11 18:23:24 +0600

    Added name for post/detail

--- posts/urls.py
+++ posts/urls.py
@@ -9,7 +9,7 @@ from .views import (
 urlpatterns = [
     path('', PostListAPIView.as_view()),
     path('post/create/', PostCreateAPIView.as_view()),
-    path('post/detail/<int:pk>/', PostDetailAPIView.as_view()),
+    path('post/detail/<int:pk>/', PostDetailAPIView.as_view(), name='detail-post'),
     path('comment/create/', CommentCreateAPIView.as_view()),
     path('comment/detail/<int:pk>/', CommentDetailAPIView.as_view())
 ]
