176d24e858a2356336308027b146595d0e944d19
Turar Arykbaev <turar.arykbaev@gmail.com>
2020-04-12 20:10:12 +0600

    image compress

--- companies/models.py
+++ companies/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from PIL import Image
+from io import BytesIO
+from django.core.files import File
+
+
+def compress(image, quality=20):
+    im_io = BytesIO()
+    im = Image.open(image)
+    im = im.convert('RGB')
+    im.save("test.jpg")
+    im.save(im_io, 'JPEG', quality=quality)
+    new_image = File(im_io, name=image.name)
+    return new_image
 
 
 class Company(models.Model):
@@ -20,15 +32,7 @@ class Company(models.Model):
         return self.company_name
 
     def save(self, *args, **kwargs):
-        self.background_photo_small = self.background_photo
-        instance = super(Company, self).save(*args, **kwargs)
-        im = Image.open(self.profile_photo.path)
-        im.save(self.profile_photo.path, quality=20, optimize=True)
-
-        im = Image.open(self.background_photo.path)
-        im.save(self.background_photo.path, quality=40, optimize=True)
-
-        im = Image.open(self.background_photo_small.path)
-        im.save(self.background_photo_small.path, quality=30, optimize=True)
-
-        return instance
+        self.profile_photo = compress(self.profile_photo)
+        self.background_photo = compress(self.background_photo, 40)
+        self.background_photo_small = compress(self.background_photo, 30)
+        super().save(*args, **kwargs)
